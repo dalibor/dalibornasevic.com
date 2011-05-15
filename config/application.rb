d7f8b0c2401d10c5d9cacc2a92a4afd560b59aa7
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-05-15 11:33:15 +0200

    Extracted secret keys in yaml file

--- config/application.rb
+++ config/application.rb
@@ -56,4 +56,9 @@ module Blog
   end
 end
 
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
 
+Recaptcha.configure do |config|
+  config.public_key  = APP_CONFIG['recaptcha']['public_key']
+  config.private_key = APP_CONFIG['recaptcha']['private_key']
+end
