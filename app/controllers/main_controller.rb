0a9d81ff24d48c3eda3f97f51e2eae8106f3fd66
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2014-07-29 22:55:58 +0200

    Remove projects from homepage

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
 
   def index
-    @posts = Post.published.order('created_at DESC').limit(5)
+    @posts = Post.published.order('created_at DESC').limit(10)
   end
 
   def show
