7733d6be8c295e18a182100996d79f2f3fca3220
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2015-03-01 22:25:15 +0100

    Remove unused method tag_cloud, add specs for posts_title

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -2,29 +2,17 @@ require 'spec_helper'
 
 describe PostsHelper do
 
-  it "should return tag name and class for 1 tag with count 1" do
-    tag1 = mock_model(Tag, :count => '1', :name => 'tag1')
-    tag_cloud([tag1], ['t1', 't2', 't3', 't4', 't5']) do |name, css_class|
-      name.should == 'tag1'
-      css_class.should == 't2'
-    end
+  it "returns posts title" do
+    posts_title.should == "All Blog Posts"
   end
 
-  it "should return tag names and classes for 5 tags" do
-    tag1 = mock_model(Tag, :count => '5', :name => 'tag1')
-    tag2 = mock_model(Tag, :count => '10', :name => 'tag2')
-    tag3 = mock_model(Tag, :count => '15', :name => 'tag3')
-    tag4 = mock_model(Tag, :count => '20', :name => 'tag4')
-    tag5 = mock_model(Tag, :count => '25', :name => 'tag5')
-
-    tags = [tag1, tag2, tag3, tag4, tag5]
-    classes = ['t1', 't2', 't3', 't4', 't5']
-    index = -1
-    tag_cloud(tags, classes) do |name, css_class|
-      index += 1
-      name.should == tags[index].name
-      css_class.should == classes[index]
-    end
+  it "returns posts title for tag" do
+    params[:tag] = 'ruby'
+    posts_title.should == "Tagged with ruby"
   end
 
+  it "returns posts title for year" do
+    params[:year] = '2014'
+    posts_title.should == "Written in 2014"
+  end
 end
